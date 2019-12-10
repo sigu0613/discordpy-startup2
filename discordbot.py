cff868426039e9e3ff5141c7c0bbac16765b5e30
sigu0613 <58355378+sigu0613@users.noreply.github.com>
2019-12-10 23:11:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ cache_limit = 300
 
 
 @client.event
-	
+async def on_message(message):	
 	if message.content.startswith("サブ垢"):
 	channel = client.get_channel(653957662609768458)
         await channel.send("キーワード検知")
