5cc2b59eaa916a95d303f087e048e1a72349cddd
sigu0613 <58355378+sigu0613@users.noreply.github.com>
2019-12-10 23:37:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,4 +20,28 @@ async def on_message(message):
 	if message.content.startswith("サブ垢"):
 		channel = client.get_channel(653957662609768458)
 		await channel.send("キーワード検知")
+@bot.event
+async def on_ready():
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+
+async def startup():
+	global bot
+	await bot.login(token, bot=True)
+	await bot.connect()
+	bot.clear()
+
+async def logout():
+	global bot
+	await bot.close()
+
+loop = asyncio.get_event_loop()
+try:
+	loop.run_until_complete(asyncio.gather(startup(), disconnect_timer()))
+except KeyboardInterrupt:
+	loop.run_until_complete(logout())
+finally:
+	loop.close()
 bot.run(token)
