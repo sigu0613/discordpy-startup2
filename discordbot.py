7b26d28e2d13c3d619f1a1a17e175635d42d977b
sigu0613 <58355378+sigu0613@users.noreply.github.com>
2019-12-11 00:11:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,13 +13,13 @@ token = os.environ['DISCORD_BOT_TOKEN']
 recruit_message = {}
 lastest_recruit_data = {}
 cache_limit = 300
-
+inform_channel_id = "653957662609768458"
 
 @client.event
 async def on_message(message):	
 	if message.content.startswith("test"):
-		channel = client.get_channel(653957662609768458)
-		await channel.send("キーワード検知")
+		channel = client.get_channel()
+		await client.send_message(inform_channel_id, "キーワード検知")
 @bot.event
 async def on_ready():
 	print('Logged in as')
