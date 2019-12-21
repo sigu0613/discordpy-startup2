713518a5406777380dbb6e0e4e74197b64cef2ce
sigu0613 <58355378+sigu0613@users.noreply.github.com>
2019-12-21 11:03:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,32 +4,17 @@ import discord
 import traceback
 import asyncio
 import random
-from googlesearch import search
+
 
 bot = commands.Bot(command_prefix='.', description='検索')
 client = discord.Client()
-
-ModeFlag = 0
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(message):
-	if message.author.bot:
-		return
-	if ModeFlag == 1:
-		kensaku = message.content
-		ModeFlag = 0
-		count = 0
-		# 日本語で検索した上位5件を順番に表示
-		for url in search(kensaku, lang="jp",num = 2):
-			await message.channel.send(url)
-			count += 1
-			if(count == 2):
-				break
-	# google検索モードへの切り替え
-	if message.content == '!google':
-		ModeFlag = 1
-		await message.channel.send('検索するワードをチャットで発言してね')
+	if message.content.startswith("test123"):
+		test = message.fetch_message(657753236606025729)
+		await message.channel.send(test)
 
 @bot.event
 async def on_ready():
